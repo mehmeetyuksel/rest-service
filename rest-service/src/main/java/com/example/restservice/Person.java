e49e59676731338b548690f74706f739b6145c68
mehmeetyuksel <67976021+mehmeetyuksel@users.noreply.github.com>
2021-03-24 21:42:58 +0300

    Add files via upload

---
+++ rest-service/src/main/java/com/example/restservice/Person.java
@@ -0,0 +1,31 @@
+package com.example.restservice;
+
+public class Person {
+	
+
+	    private int age;
+	    private String name;
+	    
+	    
+	    public String getFirstName() {
+			return name;
+		}
+	   
+	    public void setFirstName(String name) {
+			this.name = name;
+		}
+	    
+	    public int getAge() {
+			return age;
+		}
+	   
+	    public void setAge(int age) {
+			this.age = age;
+		}
+	    
+		
+
+	    // default constructor + getters + setters
+
+	}
+
